52d449266e12b44e1ffc975abbf7462dda518cfa
DeepaLaunchCode <sonaliforamit@gmail.com>
2025-07-15 23:59:39 -0500

    Base Spring boot project created with web,jpa,postgres dependency

---
+++ myfoodcafe-java-spring-back-end/src/main/java/com/myfoodcafe_java_spring_back_end/MyfoodcafeJavaSpringBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.myfoodcafe_java_spring_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfoodcafeJavaSpringBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfoodcafeJavaSpringBackEndApplication.class, args);
+	}
+
+}
