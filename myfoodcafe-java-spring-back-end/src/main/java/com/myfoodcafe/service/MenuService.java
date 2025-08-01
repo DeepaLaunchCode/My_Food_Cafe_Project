468fb830effaeb9c470cfbe2942fee3a166faec4
DeepaLaunchCode <sonaliforamit@gmail.com>
2025-07-31 21:53:13 -0500

    Reservation repository modified findByCustomerEmail feature

--- myfoodcafe-java-spring-back-end/src/main/java/com/myfoodcafe/service/MenuService.java
+++ myfoodcafe-java-spring-back-end/src/main/java/com/myfoodcafe/service/MenuService.java
@@ -4,7 +4,6 @@ import com.myfoodcafe.entity.MenuItem;
 import com.myfoodcafe.repository.MenuItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 @Service
